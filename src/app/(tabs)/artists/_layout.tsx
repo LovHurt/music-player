2dbb3956233362c2212334e03d34cbb891557fe5
lovhurt <alicandagidir@hotmail.com>
2025-03-23 17:05:21 +0300

    add artist screen layout and filtering; implement artist tracks list with search functionality and empty state handling

--- src/app/(tabs)/artists/_layout.tsx
+++ src/app/(tabs)/artists/_layout.tsx
@@ -1,7 +1,7 @@
 import { StackScreenWithSearchBar } from '@/constants/layout'
+import { colors } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
-import React from 'react'
 import { View } from 'react-native'
 
 const ArtistsScreenLayout = () => {
@@ -10,10 +10,26 @@ const ArtistsScreenLayout = () => {
 			<Stack>
 				<Stack.Screen
 					name="index"
-					options={{ ...StackScreenWithSearchBar, headerTitle: 'Artists' }}
+					options={{
+						...StackScreenWithSearchBar,
+						headerTitle: 'Artists',
+					}}
+				/>
+
+				<Stack.Screen
+					name="[name]"
+					options={{
+						headerTitle: '',
+						headerBackVisible: true,
+						headerStyle: {
+							backgroundColor: colors.background,
+						},
+						headerTintColor: colors.primary,
+					}}
 				/>
 			</Stack>
 		</View>
 	)
 }
+
 export default ArtistsScreenLayout
